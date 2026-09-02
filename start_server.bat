40854f76a32429b2715b62258f28382c27af295c
Rafael Chong <86842464+horse-3903@users.noreply.github.com>
2026-09-02 20:12:07 +0800

    Show single year instead of a range when From equals To, rename bat launcher

--- start_server.bat
+++ start_server.bat
@@ -1,7 +1,7 @@
 @echo off
 cd /d "%~dp0"
-title Grail Local Index
-echo Starting Grail Local Index server...
-start "Grail Local Index server" /min cmd /c python server.py
+title Holy Grail Mk 6 Index
+echo Starting Holy Grail Mk 6 Index server...
+start "Holy Grail Mk 6 Index server" /min cmd /c python server.py
 timeout /t 2 /nobreak >nul
 start "" http://127.0.0.1:8765/
